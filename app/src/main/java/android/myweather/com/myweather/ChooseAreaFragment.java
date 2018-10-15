8d11e3dbf0dfa10b39364f47835b90223dbfbcb1
plz <panzer190@163.com>
2018-10-15 08:44:00 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/android/myweather/com/myweather/ChooseAreaFragment.java
+++ app/src/main/java/android/myweather/com/myweather/ChooseAreaFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.myweather.com.myweather.db.City;
 import android.myweather.com.myweather.db.County;
 import android.myweather.com.myweather.db.Province;
+import android.myweather.com.myweather.gson.Weather;
 import android.myweather.com.myweather.util.HttpUtil;
 import android.myweather.com.myweather.util.Utility;
 import android.nfc.Tag;
@@ -88,10 +89,17 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY){
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof  MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
